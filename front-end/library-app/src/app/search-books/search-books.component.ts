26dbfba8fc33d74808bc717740a31d502e56b6c3
AX15\Chad <chad206548@gmail.com>
2024-08-15 10:52:32 -0500

    Implemented filtering on the search page to mimic the featured page functionality

--- front-end/library-app/src/app/search-books/search-books.component.ts
+++ front-end/library-app/src/app/search-books/search-books.component.ts
@@ -2,27 +2,40 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableModule, MatTable } from '@angular/material/table';
 import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
 import { MatSortModule, MatSort } from '@angular/material/sort';
-import {
-  BooksViewModelDataSource,
-  
-} from '../featured-books/books-datasource';
+import { BooksViewModelDataSource } from '../featured-books/books-datasource';
 import { DataService } from '../services/data.service';
 import { Router } from '@angular/router';
 import { BookViewModel } from '../interfaces';
+import { CommonModule } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-books',
   templateUrl: './search-books.component.html',
   styleUrl: './search-books.component.scss',
   standalone: true,
-  imports: [MatTableModule, MatPaginatorModule, MatSortModule],
+  imports: [
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    CommonModule,
+    MatButton,
+    MatIcon,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
 })
 export class SearchBooksComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
   @ViewChild(MatTable) table!: MatTable<BookViewModel>;
   dataSource = new BooksViewModelDataSource([]);
-
+  searchMode = true;
+  searchForm: FormGroup = new FormGroup({
+    searchText: new FormControl(''),
+  });
   constructor(private dataService: DataService, private router: Router) {}
 
   /** Columns displayed in the table. Columns IDs can be added, removed, or reordered. */
@@ -41,4 +54,21 @@ export class SearchBooksComponent implements OnInit {
   viewBook(bookId: number) {
     this.router.navigate(['/book', bookId]);
   }
+
+  cancel() {
+    this.searchMode = true;
+    var ctrl = this.searchForm?.controls['searchText'];
+    ctrl.setValue('');
+    this.filter('');
+  }
+
+  search() {
+    this.searchMode = false;
+    var ctrl = this.searchForm?.controls['searchText'];
+    this.filter(ctrl.value);
+  }
+
+  filter(searchText: string) {
+    this.table.dataSource = this.dataSource.data.filter((x) => x.title.includes(searchText));
+  }  
 }
